663dcc1a8c6124f1ed53a085d898eaa3b9007662
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-11-05 19:47:36 -0400

    get_keychain_pass

--- .mutt/offlineimap.py
+++ .mutt/offlineimap.py
@@ -1,1 +1,8 @@
-import keyring
+#!/usr/bin/python
+
+import re, subprocess
+
+def get_keychain_pass(account=None):
+    command = "pass " + account
+    return subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+
