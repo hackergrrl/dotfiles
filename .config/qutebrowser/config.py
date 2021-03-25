28f7534b1a74a5e5f9854e97f74c0df8bb52201d
Kira Oakley <kira@eight45.net>
2021-03-24 18:36:10 -0700

    various

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,11 @@
 config.bind('<Ctrl-o>', 'spawn chromium {url}')
 config.bind('Y', 'hint links yank')
 config.bind('I', 'hint images run open -t {hint-url}')
+config.bind('W', 'hint links spawn chromium https://web.archive.org/web/*/{hint-url}')
+config.bind('T', 'spawn st /home/sww/bin/turl {url}')
+
+## Maybe only available in new qutebrowser versions?
+# config.set("colors.webpage.darkmode.enabled", True)
+
+# how to do this? so i can fill in a package name at the end
+#config.bind('pn', 'open -t https://npmjs.org/package/')
